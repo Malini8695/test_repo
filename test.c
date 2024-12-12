ef9f364a5109ab6d9e22a0a874e9f603fa9e8432
Malini8695 <118871729+Malini8695@users.noreply.github.com>
2024-12-12 16:03:09 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,9 +1,7 @@
-//branch 2
+//This is branch1
 //going to merge from branch 2 to branch 1
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
    printf("Hello, World!");
    return 0;
-}
-
